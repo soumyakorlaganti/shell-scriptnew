3413660deee76ee9cc0b3f3cf3e9ca3e8404db30
soumya <korlagani.soumya@gmail.com>
2025-12-01 18:51:40 +0530

    hello world

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=raju
+PERSON2=sai
+
+echo "$PERSON1:: Hello $PERSON2 , how are you?"
+echo "$PERSON2:: Hi $PERSON1 ,I am fine. How are you?"
+echo "$PERSON1:: I am fine too , how is your work?"
+echo "$PERSON2:: Not bad, I am thinking to upgrade to devops"
