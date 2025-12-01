1d137d1badbffb0629739b9b54af8193f35ff07f
soumya <korlagani.soumya@gmail.com>
2025-12-01 19:11:52 +0530

    hello world

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,10 +2,10 @@
 
 echo "Please enter username::"
 
-read USERNAME
+read -s USERNAME
 
 echo "Please enter password::"
  
-read PASSWORD
+read -s PASSWORD
 
 echo "username is: $USERNAME , password is: $PASSWORD"
