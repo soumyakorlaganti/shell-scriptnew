1a0c3f3fb3315e90c1451be43e6c82caf9213e0d
soumya <korlagani.soumya@gmail.com>
2025-12-01 18:39:45 +0530

    hello world

--- 02-varibles.sh
+++ 02-varibles.sh
@@ -1,1 +1,6 @@
-this is again sample script.
+#!/bin/bash
+
+echo "raju:: Hello sai, How are you?"
+echo "sai:: Hi raju, I am fine. How are you?"
+echo "raju:: I am fine too. How is your work?"
+echo "sai:: Not bad.I am thinking to upgrade to devops"
