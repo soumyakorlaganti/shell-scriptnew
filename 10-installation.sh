ef27ddb1647e7f7be504ce69b32ecb53ae4ffed1
soumya <korlagani.soumya@gmail.com>
2025-12-01 20:11:58 +0530

    hello script

---
+++ 10-installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USERID=$(id-u)
+ 
+if [ $USERID -eq 0 ]
+
+then 
+    echo "Please run the script with root access."
+else
+    echo "you are super user"
+fi
+
+dnf install mysql -y
+
