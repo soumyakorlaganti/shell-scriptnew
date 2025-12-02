36ca4fd0e935aa56d08ab1eb56162d4e7a1b2ff7
soumya <korlagani.soumya@gmail.com>
2025-12-02 14:10:34 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,6 +2,8 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -21,8 +23,8 @@ else
     echo "You are super user."
 fi
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL"
 
-dnf install git -y
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing Git"
