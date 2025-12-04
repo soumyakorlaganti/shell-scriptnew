9bff2402d764376a4731283d89d3a21d2d882d9a
soumya <korlagani.soumya@gmail.com>
2025-12-04 19:24:08 +0530

    cron

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -5,10 +5,10 @@ DISK_THRESHOLD=6
 
 while IFS= read -r line
 do
-    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        echo "$FOLDER is more than $DISK_THRESHOULD, Current usage: $USAGE"
+        echo "$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE"
     fi
 done <<< $DISK_USAGE
