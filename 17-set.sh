82de013cd4ff2bf46ee584e45ecfba33122530ad
soumya <korlagani.soumya@gmail.com>
2025-12-04 13:04:18 +0530

    calling script

---
+++ 17-set.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e 
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run the script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysql -y
+dnf install git -y
+
+echo "is script proceeding?"
+
