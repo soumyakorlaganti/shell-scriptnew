93cf5989e4a1c2d7ce59dff0b33d309c5c7e53f0
soumya <korlagani.soumya@gmail.com>
2025-12-05 18:45:16 +0530

    memory

--- 20-ram-memory.sh
+++ 20-ram-memory.sh
@@ -4,18 +4,21 @@ THRESHOLD=100
 #ALERT_EMAIL="soumyamunni510@gmail.com"
 MESSAGE=" "
 
-USED=$(free | grep Mem | awk '{print $3}')
-TOTAL=$(free | grep Mem | awk '{print $2}')
 
-USAGE=$(( USED * 100 / TOTAL ))
+while IFS= read -r line
+do
+    USED=$(free | grep Mem | awk '{print $3}')
+    TOTAL=$(free | grep Mem | awk '{print $2}')
+
+    USAGE=$(( USED * 100 / TOTAL ))
 
 
-echo  "Current RAM Usage: $USAGE"
-if [ $USAGE -ge $THRESHOLD ]
-then
-    MESSAGE+="$USAGE is more than $THRESHOLD, Current usage: $USED \n"
-fi
-
+    echo  "Current RAM Usage: $USAGE"
+    if [ $USAGE -ge $THRESHOLD ]
+    then
+        MESSAGE+="$USAGE is more than $THRESHOLD, Current usage: $USED \n"
+    fi
+done <<< $THERSHOLD
 
 echo -e "Message: $MESSAGE"
 
