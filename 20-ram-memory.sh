3e1a50c88fa9ed72b0508308652da31310eaaaf7
soumya <korlagani.soumya@gmail.com>
2025-12-05 19:15:31 +0530

    memory

--- 20-ram-memory.sh
+++ 20-ram-memory.sh
@@ -9,6 +9,9 @@ USED=$(free | grep Mem | awk '{print $3}')
 TOTAL=$(free | grep Mem | awk '{print $2}')
 
 USAGE=$(( USED * 100 / TOTAL ))
+
+echo "Current RAM Usage: $USAGE%"
+
 if [ $USAGE -ge $THRESHOLD ]
 then
     MESSAGE+="$USAGE is more than $THRESHOLD, Current usage: $USAGE \n"
