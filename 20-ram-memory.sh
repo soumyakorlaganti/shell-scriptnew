75471465339f6ae96c2ef528e877c988ab5601a0
soumya <korlagani.soumya@gmail.com>
2025-12-05 18:58:20 +0530

    memory

--- 20-ram-memory.sh
+++ 20-ram-memory.sh
@@ -14,7 +14,7 @@ do
 
     if [ $USAGE -ge $THRESHOLD ]
     then
-        MESSAGE+="$USED is more than $TOTAL, Current usage: $USED \n"
+        MESSAGE+="$USAGE is more than $TOTAL, Current usage: $USAGE \n"
     fi
 done <<< $THERSHOLD
 
