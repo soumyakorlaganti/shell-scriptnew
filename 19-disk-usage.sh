fe815097af8afe64f76e3f8e9365b33fae7e6be1
soumya <korlagani.soumya@gmail.com>
2025-12-05 15:21:37 +0530

    cron

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -3,7 +3,7 @@
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=6
 
-while IFS=read -r line
+while IFS= read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
