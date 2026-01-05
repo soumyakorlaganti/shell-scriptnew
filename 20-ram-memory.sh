5c4e73fcddd57f3b93af5080a76749ad56c3b272
soumya <korlagani.soumya@gmail.com>
2026-01-05 18:20:37 +0530

    terraform

--- 20-ram-memory.sh
+++ 20-ram-memory.sh
@@ -1,28 +1,3 @@
-# #!/bin/bash
-
-# THRESHOLD=100
-# #ALERT_EMAIL="soumyamunni510@gmail.com"
-# MESSAGE=""
-
-
-# USED=$(free | grep Mem | awk '{print $3}')
-# TOTAL=$(free | grep Mem | awk '{print $2}')
-
-# USAGE=$(( USED * 100 / TOTAL ))
-
-# echo "Current RAM Usage: $USAGE%"
-
-# if [ $USAGE -ge $THRESHOLD ]
-# then
-#     MESSAGE+="$USAGE is more than $THRESHOLD, Current usage: $USAGE \n"
-# fi
-
-
-# echo -e "Message: $MESSAGE"
-
-
-# echo -e "$MESSAGE" | mail -s "Memory Threshold Alert" soumyamunni510@gmail.com
-
 #!/bin/bash
 
 THRESHOLD=80
@@ -34,24 +9,10 @@ TOTAL=$(free | awk '/Mem:/ {print $2}')
 USAGE=$(( USED * 100 / TOTAL ))
 
 echo -e "Message: $MESSAGE Current RAM Usage: $((USED/1024)) MB"
-# echo  -e "Current RAM Usage: $((USED/1024)) MB"
 
 if [ $USED -ge $THRESHOLD ]
 then
     MESSAGE+="Current RAM Usage $((USED/1024)) MB is more than $THRESHOLD"
 fi
-
-# if [ $USAGE -ge $THRESHOLD ]; then
-#     MESSAGE+="RAM usage alert!\n"
-#     MESSAGE+="Current Usage : $USAGE% \n"
-#     MESSAGE+="Used Memory   : $((USED/1024)) MB\n"
-#     MESSAGE+="Total Memory  : $((TOTAL/1024)) MB\n"
-#     MESSAGE+="Threshold     : $THRESHOLD% \n"
-# fi
-# if [ -n "$MESSAGE" ]; then
-#     echo -e "Message: $MESSAGE"
-#     echo "$MESSAGE" | mail -s "Memory Threshold Alert" soumyamunni510@gmail.com
-# fi
-# echo -e "$MESSAGE" | mail -s "Memory Threshold Alert" soumyamunni510@gmail.com
 
 echo "$MESSAGE" | mail -s "Current RAM Usage" soumyamunni510@gmail.com
