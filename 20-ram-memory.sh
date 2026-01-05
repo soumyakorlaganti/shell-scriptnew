142cd2dbebff743683ff4b0d7d70b98ea59b4baf
soumya <korlagani.soumya@gmail.com>
2026-01-05 16:14:45 +0530

    terraform

--- 20-ram-memory.sh
+++ 20-ram-memory.sh
@@ -33,7 +33,7 @@ TOTAL=$(free | awk '/Mem:/ {print $2}')
 
 USAGE=$(( USED * 100 / TOTAL ))
 
-echo "Current RAM Usage: $USAGE%"
+echo "Current RAM Usage: $USAGE% \n"
 
 if [ $USAGE -ge $THRESHOLD ]; then
     MESSAGE+="RAM usage alert!\n"
