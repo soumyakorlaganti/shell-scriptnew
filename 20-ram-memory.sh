1abfdaf2a565aee3ba6be3fb21b7b39d232c6eb4
soumya <korlagani.soumya@gmail.com>
2026-01-05 16:23:14 +0530

    terraform

--- 20-ram-memory.sh
+++ 20-ram-memory.sh
@@ -45,7 +45,6 @@ if [ $USAGE -ge $THRESHOLD ]; then
 fi
 
 
-
 if [ -n "$MESSAGE" ]; then
-    echo -e "$MESSAGE" | mail -s "Memory Threshold Alert" "soumyamunni510@gmail.com"
+    echo "$MESSAGE" | mail -s "Memory Threshold Alert" soumyamunni510@gmail.com
 fi
